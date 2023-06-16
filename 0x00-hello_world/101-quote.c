bf8a1ae1b3ba3801b1eecffd4e7e16259f1f3063
SAAJEVES <eveshofase@gmail.com>
2023-06-15 19:32:18 -0700

    quote3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
- * Return: The program returns 1
+ * Return: The program returns (1)
  */
 
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
 	return (1);
 }
