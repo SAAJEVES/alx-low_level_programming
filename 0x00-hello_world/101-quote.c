29909ace03121b1cf1cafc27ce29388ee8291bce
SAAJEVES <eveshofase@gmail.com>
2023-06-15 19:39:32 -0700

    quote5

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - prints the  and that piece of art is useful" - Dora Korpar, 2015-10-19
  * Return: The program returns (1)
  */
 
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, " 
-	"2015-10-19);
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
