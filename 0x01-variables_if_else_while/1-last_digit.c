d7430d36c672bd209e53ac9e74db0c59e46369c1
SAAJEVES <eveshofase@gmail.com>
2023-06-17 00:41:49 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,10 @@
 
 /**
  * Main - entry point
- * Description - carries out a chck on the last digit of genreated number
+ *
+ * Description : carries out a check on the last digit of generated
+ *	number
+ *
  * Return: always 0 (Success)
  */
 
@@ -15,20 +18,23 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int lastDigit = n % 10;
+	int lstNum = n % 10;
 
 	if (n < 6 && n > 0)
 	{
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, lstNum);
 	}
 	else if (n > 5)
 	{
-		printf("last digit of %d is %d and is  greater than 5\n", n, lastDigit);
+		printf("last digit of %d is %d and is  greater than 5\n", n, lstNum);
 	}
 	else if (n == 0)
 	{
-		printf("last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("last digit of %d is %d and is 0\n", n, lstNum);
 	}
+	else
+	{
+		printf("last digit of %d is %d and is less than 0\n", n, lstNum);
 
 
 	return (0);
