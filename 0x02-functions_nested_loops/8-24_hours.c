ec0feed959dc7e7ed6715af19c7c519fac44590f
SAAJEVES <eveshofase@gmail.com>
2023-06-21 20:20:49 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int min;
+
+	hour = 0;
+	while (hour < 24)
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			min++;
+		}
+		hour++;
+	}
+	return (0);
+}
