af7bd53f52929c34e580b50d49b31b7a104d6cdb
SAAJEVES <eveshofase@gmail.com>
2023-06-27 07:23:35 -0700

    times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,16 @@
+#ifndef main_h
+#define main_h
+int _putchar(char c);
 void print_alphabet(void);
-void _putchar(char a);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int a);
-int print_last_digit(int a);
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
-
+void print_times_table(int n);
+#endif
