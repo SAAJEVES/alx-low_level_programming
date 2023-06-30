1f5e2697c43ee1b63f94c03799c73d3bf651e172
SAAJEVES <eveshofase@gmail.com>
2023-06-30 14:51:48 -0700

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_toupper - changes lowercase to upper
+ *
+ * @a: the string to captalize
+ *
+ * Return: a string
+ */
+char *string_toupper(char *a)
+{
+	int i;
+	int stringLen;
+
+	size_t lent = sizeof(a);
+
+	stringLen = (int)lent;
+	for (i = 0; i < stringLen; i++)
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			a[i] = a[i] - 32;
+		}
+		else
+		{
+			a[i] = a[i];
+		}
+	}
+	return (a);
+}
