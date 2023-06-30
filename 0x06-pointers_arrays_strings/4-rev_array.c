eff2b2cfc063ae8dbfca630e76d32cdef71ffae8
SAAJEVES <eveshofase@gmail.com>
2023-06-30 02:15:37 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * reverse_array - reverses an array
+ *
+ * @a: the array
+ *
+ * @n: the length of an array;
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+	int* arrCpy;
+
+	arrCpy = malloc(n * sizeof(int));
+	if (arrCpy == NULL)
+	{
+		free(arrCpy);
+	}
+
+	i = n - 1;
+
+	for (j = 0; j < n; j++)
+	{
+		arrCpy[j] = a[j];
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		a[j] = arrCpy[i];
+		i--;
+	}
+	free(arrCpy);
+}
