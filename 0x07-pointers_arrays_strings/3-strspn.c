64615ec9a3dea7673892b30538a887fb3473904d
SAAJEVES <eveshofase@gmail.com>
2023-07-03 18:50:06 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * _strspn - checks for a substring in a string
+ * @s: the string
+ * @accept: the substring
+ *
+ * Return: returns an unsined int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int times;
+	int i;
+	int j;
+
+	times = 0;
+	i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+
+		while (*(accept + j) != '\0')
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				times++;
+				break;
+			}
+		}
+		if (*(s + i) != *(accept + j))
+		{
+			break;
+		}
+	}
+	return (times);
+}
