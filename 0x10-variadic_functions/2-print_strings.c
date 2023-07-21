8b2611fb55e38a5e600a4a3dd3f616b8a8b79b00
SAAJEVES <eveshofase@gmail.com>
2023-07-20 18:39:28 -0700

    print strings2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		word = va_arg(words, char *);
-		if (word == NULL)
+		if (!word)
 		{
 			printf("(nil)");
 		}
