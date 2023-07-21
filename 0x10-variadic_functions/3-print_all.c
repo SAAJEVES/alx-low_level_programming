b9a1ca4e73b06def9cb952610a303fc0a50b3692
SAAJEVES <eveshofase@gmail.com>
2023-07-21 07:58:23 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * printChar - print char
+ * @form: form
+ * Return: void
+ */
+void printChar(va_list form)
+{
+	int c = va_arg(form, int);
+	printf("%c", c);
+}
+
+/**
+ * printInt - print inf
+ * @form: form
+ * Return: void
+ */
+void printInt(va_list form)
+{
+	int num = va_arg(form, int);
+	printf("%d", num);
+}
+
+/**
+ * printFloat - print float
+ * @form: form
+ * Return: void
+ */
+void printFloat(va_list form)
+{
+	float num = va_arg(form, double);
+	printf("%f", num);
+}
+
+/**
+ * printString - print string literals
+ * @form: form
+ * Return: void
+ */
+void printString(va_list form)
+{
+	char *value = va_arg(form, char *);
+	if (!value)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", value);
+	}
+}
+
+/**
+ * print_all - print all
+ * @format: format specifier
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list form;
+	type_print value[] = {
+		{"c", printChar},
+		{"s", printString},
+		{"f", printFloat},
+		{"i", printInt},
+		{NULL, NULL},
+	};
+	int i, j;
+	char *separator;
+
+	separator = "";
+	i = 0;
+	j = 0;
+
+	va_start(form, format);
+	while (format[i] && format)
+	{
+		while (value[j].s)
+		{
+			if (*value[j].s == format[i])
+			{
+				printf("%s", separator);
+				value[j].func(form);
+				separator = ", ";
+			}
+			++j;
+		}
+		j = 0;
+		++i;
+	}
+	printf("\n");
+	va_end(form);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,19 @@
 #ifndef VARIADIC
 #define VARIADIC
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct typePrint
+{
+	char *s;
+	void (*func)(va_list form);
+} type_print;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-#endif
+#endif /* VARIADIC */
