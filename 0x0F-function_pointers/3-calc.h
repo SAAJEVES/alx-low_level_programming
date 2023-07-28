fe8c03b3fbc76a988eac4855214c3e95dce7f846
SAAJEVES <eveshofase@gmail.com>
2023-07-27 20:13:35 -0700

    code

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef SAAJ
+#define SAAJ
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+int (*get_op_func(char *s))(int, int);
+
+#endif
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: return an integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: return an integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: return an integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: return an integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: return an integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
