e199e60bfbf257782b5a7fe35296d72962f7de83
SAAJEVES <eveshofase@gmail.com>
2023-07-27 20:57:08 -0700

    Strange

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void saySomething(void) __attribute__((constructor));
  */
 void saySomething(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow, \n"
+			"I bore my house upon my back!\n");
 }
