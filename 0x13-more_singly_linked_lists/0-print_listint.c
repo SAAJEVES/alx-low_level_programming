cfe8e49baa25a8512d1cb609ef769bd7349e77c1
SAAJEVES <eveshofase@gmail.com>
2023-08-01 04:33:03 -0700

    print

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list
+ * @h: the header pointer
+ * Return: a size_t data type ie the number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	i = 0;
+
+	if (h == NULL)
+	{
+		return (i);
+	}
+	else
+	{
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+## More Singly Linked List
+
+This readme file contains the function for each c programming file
+
+1) 0-print_listint.c:
