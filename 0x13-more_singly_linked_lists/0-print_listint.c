68bd8758d03aaaa01fbbb6460781605fca52eba8
SAAJEVES <eveshofase@gmail.com>
2023-08-01 17:24:49 -0700

    print3

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,22 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i;
+	size_t nodeCount = 0;
 
-	i = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodeCount++;
+	}
 
-	if (h == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		while (h != NULL)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			i++;
-		}
-	}
-	return (i);
+	return (nodeCount);
 }
