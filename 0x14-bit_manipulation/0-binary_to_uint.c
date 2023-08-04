98528c81ed3738967b0ba3e964577486f1389e43
SAAJEVES <eveshofase@gmail.com>
2023-08-03 18:38:30 -0700

    binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * custom_pow - work in place of pow
+ * base: 2
+ * exponent: any value greater 0
+ * Return: a value
+ */
+unsigned int custom_pow(unsigned int base, unsigned int exponent) {
+	unsigned int result = 1;
+
+	while (exponent > 0) {
+		result *= base;
+		exponent--;
+	}
+
+	return result;
+}
+
+
+
+/**
+ * binary_to_uint - converst binary to decimal
+ * @b: string literal
+ * Return: an unsigned intger
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec, noVal, val, powered, base, power;
+	int i;
+
+	noVal = 0;
+	dec = 0;
+	i = 0;
+	base = 2;
+
+	if (b == NULL)
+		return (noVal);
+
+	power = (unsigned int)strlen(b);
+
+	while (b[i] != '\0')
+	{
+		power--;
+		if (b[i] > '1' || b[i] < '0')
+		{
+			return (noVal);
+		}
+		val = (unsigned int) (b[i] - '0');
+		powered = custom_pow(base, power);
+		dec += val * powered;
+		i++;
+	}
+	return (dec);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,12 @@
-ifndef MAIN_H
-define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 unsigned int binary_to_uint(const char *b);
 
 
-endif /* MAIN_H */
+#endif /* MAIN_H */
