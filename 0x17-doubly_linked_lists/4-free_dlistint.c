86d0bf483de775a8319317f3ccf89d2138cd8298
SAAJEVES <eveshofase@gmail.com>
2023-09-15 08:39:45 +0100

    free nodes

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - free the nodes in a linked list
+ *
+ * @head: linked list
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	
+	temp = head;
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
