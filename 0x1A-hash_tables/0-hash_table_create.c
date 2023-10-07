a12fcd3e79c17a531e385b1f0ab7c134eb83e519
SAAJEVES <eveshofase@gmail.com>
2023-10-07 01:22:42 +0100

    create hashtable1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,5 +20,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
-	return table;
+	return (table);
 }
