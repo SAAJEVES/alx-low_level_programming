ad04f3168a1bbe7ddf9c289abeb31f1c59d04198
SAAJEVES <eveshofase@gmail.com>
2023-10-07 04:07:31 +0100

    ht betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,3 +1,26 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_get - get value implementation
+ *
+ * @ht: table
+ *
+ * @key: key
+ *
+ * Return: value ie a char * or NULL
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int indexOfKey;
+
+	if (strlen(key) < 1 || ht == NULL || key == NULL)
+		return (NULL);
+
+	indexOfKey = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[indexOfKey] == NULL)
+		return (NULL);
+
+	if (indexOfKey > ht->size)
+		return (NULL);
+	return (ht->array[indexOfKey]->value);
+}
