892f79b7c5a3a45faf6e4e13cb71713ba7eced55
SAAJEVES <eveshofase@gmail.com>
2023-10-07 03:14:27 +0100

    betty cool

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - sets up the hash table
+ *
+ * @ht: the table
+ *
+ * @key: the key
+ *
+ * @value: the value
+ *
+ * Return: Success 1 while Failure 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int indexOfKey;
+	hash_node_t *node;
+	const unsigned char *newKey;
+
+	if (strlen(key) < 1)
+		return (0);
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	newKey = (const unsigned char *)key;
+	indexOfKey = key_index(newKey, ht->size);
+	node->key = (char *)key;
+	node->value = (char *)value;
+	if (ht->array[indexOfKey] == NULL)
+	{
+		ht->array[indexOfKey] = node;
+		node->next = ht->array[indexOfKey + 1];
+	}
+	else
+	{
+		ht->array[0] = node;
+		node->next = ht->array[1];
+	}
+	return (1);
+}
